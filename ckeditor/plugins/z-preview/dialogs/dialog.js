9b1742e7fec8ee385789dd6c5ec9665a65e196a6
owjosh <joshua@getopenwater.com>
2020-08-06 18:51:59 -0500

    x

--- ckeditor/plugins/z-preview/dialogs/dialog.js
+++ ckeditor/plugins/z-preview/dialogs/dialog.js
@@ -6,63 +6,16 @@ CKEDITOR.dialog.add('z-preview-dialog', function(editor) {
         contents: [{
             id: 'z-preview-config',
             elements: [{
-                    setup: function(widget) {
-                        this.setValue(widget.data.bg_color);
-                    },
-                    commit: function(widget) {
-                        widget.setData('bg_color', this.getValue());
-                    },
-                    type: 'text',
-                    id: 'bg_color',
-                    label: 'Background color (HEX, RGB, RGBA) or leave blank for default'
+                setup: function(widget) {
+                    this.setValue(widget.data.bg_color);
                 },
-                {
-                    setup: function(widget) {
-                        this.setValue(widget.data.text_color);
-                    },
-                    commit: function(widget) {
-                        widget.setData('text_color', this.getValue());
-                    },
-                    type: 'text',
-                    id: 'text_color',
-                    label: 'Text color (HEX, RGB, RGBA) or leave blank for default'
+                commit: function(widget) {
+                    widget.setData('bg_color', this.getValue());
                 },
-                {
-                    setup: function(widget) {
-                        this.setValue(widget.data.link_url);
-                    },
-                    commit: function(widget) {
-                        var value = this.getValue();
-                        if (value.indexOf('http') !== 0 && value.indexOf('mailto:') !== 0) value = 'http://' + value;
-                        widget.setData('link_url', value);
-                    },
-                    type: 'text',
-                    id: 'link_url',
-                    label: 'Link URL with full path, http or https required'
-                },
-                {
-                    setup: function(widget) {
-                        this.setValue(widget.data.link_text);
-                    },
-                    commit: function(widget) {
-                        widget.setData('link_text', this.getValue());
-                    },
-                    type: 'text',
-                    id: 'link_text',
-                    label: 'Link text to display on page'
-                },
-                {
-                    setup: function(widget) {
-                        this.setValue(widget.data.new_tab)
-                    },
-                    commit: function(widget) {
-                        widget.setData('new_tab', this.getValue())
-                    },
-                    type: 'checkbox',
-                    id: 'new_tab',
-                    label: 'Open link in a new tab?'
-                }
-            ]
+                type: 'text',
+                id: 'bg_color',
+                label: 'Background color (HEX, RGB, RGBA) or leave blank for default'
+            }]
         }]
     };
 });
--- ckeditor/plugins/z-preview/plugin.js
+++ ckeditor/plugins/z-preview/plugin.js
@@ -14,21 +14,10 @@ CKEDITOR.plugins.add('z-preview', {
             init: function() {
                 // initialize widget data object
                 this.setData('bg_color', this.element.getStyle('background-color'));
-                this.setData('text_color', this.element.getStyle('color'));
-                this.setData('link_url', this.element.getAttribute('href'));
-                this.setData('link_text', this.element.getText());
             },
             data: function() {
                 // update widget with new data from dialog
                 this.element.setStyle('background-color', this.data.bg_color);
-                this.element.setStyle('color', this.data.text_color);
-                this.element.setAttribute('data-cke-saved-href', this.data.link_url);
-                this.element.setText(this.data.link_text);
-
-                if (this.data.new_tab)
-                    this.element.setAttribute('target', '_blank');
-                else
-                    this.element.removeAttribute('target');
             }
         });
 
