84685475cb9f4992185656c1fbc64e2d46b9dd57
owjosh <joshua@getopenwater.com>
2020-08-12 08:10:10 -0500

    disable change program

--- custom/disableChangeProgram.js
+++ custom/disableChangeProgram.js
@@ -1,6 +1,5 @@
-$("body").css({ backgroundColor: "yellow" });
-
 $(".changeSolicitation").hide();
+$("div.currentSolicitation > div").hide();
 $.get('/a/organizations/main/solicitations/selectcurrentsolicitation', function(data) {
     $(data).find("script:contains('currentSolicitationId')").each(function() {
         var checkers = $(this).text().slice(127, 129);;
