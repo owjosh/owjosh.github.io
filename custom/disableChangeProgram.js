6b36d7fbdd31fe7c5b1e453807b28dab61ff4fe1
owjosh <joshua@getopenwater.com>
2020-08-12 10:04:43 -0500

    x

--- custom/disableChangeProgram.js
+++ custom/disableChangeProgram.js
@@ -2,11 +2,14 @@ $(".changeSolicitation").parents("li").hide();
 $(".currentSolicitation").hide();
 $.get('/a/organizations/main/solicitations/selectcurrentsolicitation', function(data) {
     $(data).find("script:contains('currentSolicitationId')").each(function() {
-        var checkers = $(this).text().slice(127, 129);;
+        var checkers = $(this).text().slice(127, 129);
         if (checkers != '""') {
             Backbone.post('/a/organizations/main/solicitations/selectcurrentsolicitation', {
-                solicitationId: ''
-            })
+                    solicitationId: ''
+                }, { dontShowSuccessAlert: !0 }, )
+                .done(function(e) {
+                    window.location.assign(window.location.href)
+                })
         }
     })
 });
