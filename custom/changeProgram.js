31335883ab05d824686b7aff0c069ff365f7e765
joshua wray <joshua@getopenwater.com>
2020-08-12 07:53:28 -0500

    x

--- custom/changeProgram.js
+++ custom/changeProgram.js
@@ -1,6 +1,8 @@
+$("body").css({ backgroundColor: "yellow" });
+
+$(".changeSolicitation").hide();
 $.get('/a/organizations/main/solicitations/selectcurrentsolicitation', function(data) {
     $(data).find("script:contains('currentSolicitationId')").each(function() {
-        $("#wl_msg").text('hhhhhh');
         var checkers = $(this).text().slice(127, 129);;
         if (checkers != '""') {
             Backbone.post('/a/organizations/main/solicitations/selectcurrentsolicitation', {
@@ -8,5 +10,4 @@ $.get('/a/organizations/main/solicitations/selectcurrentsolicitation', function(
             })
         }
     })
-
 });
