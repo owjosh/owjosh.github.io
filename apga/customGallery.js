44e85f2990b9d165e73908ec6778e386fdd6b3f6
Joshua Wray <joshua@getopenwater.com>
2020-09-04 18:45:56 -0500

    no message

--- apga/customGallery.js
+++ apga/customGallery.js
@@ -12,6 +12,7 @@ function hideAllSessions() {
 if ($(".gallery_col").length != 0) {
     $("#no_results").hide();
 };
+
 if (window.location.toString().includes("roundId=2&")) {
     $("#tabs, #containerContent, div.applicationGallery_info > p:nth-child(4), div.applicationGallery_info > hr").hide();
     $("#gallery_back").show();
