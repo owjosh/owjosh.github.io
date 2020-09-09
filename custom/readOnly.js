0b067e2fb21095b5ace93443802371fd18c47679
Joshua Wray <joshua@getopenwater.com>
2020-09-09 16:07:23 -0500

    no message

--- custom/readOnly.js
+++ custom/readOnly.js
@@ -1,18 +1,17 @@
-var readOnlyCss = document.createElement('link');
-readOnlyCss.rel = 'stylesheet';
-readOnlyCss.type = 'text/css';
-readOnlyCss.href = 'https://owjosh.github.io/custom/read-only.css';
-document.getElementsByTagName('HEAD')[0].appendChild(readOnlyCss);
+// jshint esversion: 6
+$('head').append($('<' + 'link rel="stylesheet" type="text/css" />').attr('href', 'https://owjosh.github.io/custom/read-only.css'));
 
 $(function() {
     $.getScript("https://openwater-themes.s3.amazonaws.com/js/waitForElement.js", function(data, textStatus, jqxhr) {
-        console.log(data); //data returned
-        console.log(textStatus); //success
-        console.log(jqxhr.status); //200
-        console.log('Load was performed.');
-        $(".read-only input[type='text'], .read-only textarea, .read-only select").waitUntilExists(function() {
+        $('.read-only select').waitUntilExists(function() {
+            $('.read-only select').attr("disabled", true);
+            this.tabIndex = -1;
+            console.log('select readonly');
+        })
+        $(".read-only input[type='text'], .read-only textarea").waitUntilExists(function() {
             this.readOnly = true;
             this.tabIndex = -1;
-        });
+            console.log('basic readonly');
+        })
     });
 });
