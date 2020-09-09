464cc2414184c8be41b5dc8af4f7ee17bdc797bc
Joshua Wray <joshua@getopenwater.com>
2020-09-09 14:22:23 -0500

    no message

--- custom/readOnly.js
+++ custom/readOnly.js
@@ -4,10 +4,11 @@ readOnlyCss.type = 'text/css';
 readOnlyCss.href = 'https://owjosh.github.io/custom/read-only.css';
 document.getElementsByTagName('HEAD')[0].appendChild(readOnlyCss);
 
-$.ajax({
-    url: "https://openwater-themes.s3.amazonaws.com/js/waitForElement.js",
-    dataType: "script",
-    success: success
+$.getScript("https://openwater-themes.s3.amazonaws.com/js/waitForElement.js", function(data, textStatus, jqxhr) {
+    console.log(data); //data returned
+    console.log(textStatus); //success
+    console.log(jqxhr.status); //200
+    console.log('Load was performed.');
 });
 
 $(function() {
