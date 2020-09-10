8154f687cd0b7598433c3f9370ba720c8c4133c3
Joshua Wray <joshua@getopenwater.com>
2020-09-09 20:09:20 -0500

    no message

--- custom/readOnly.js
+++ custom/readOnly.js
@@ -3,7 +3,7 @@ $('head').append($('<' + 'link rel="stylesheet" type="text/css" />').attr('href'
 $(function() {
     $.getScript("https://openwater-themes.s3.amazonaws.com/js/waitForElement.js", function() {
         $('.read-only select').waitUntilExists(function() {
-            $('.read-only select').attr = ("disabled", true);
+            $('.read-only select').attr("disabled", true);
             this.tabIndex = -1;
         })
         $(".read-only input[type='text'], .read-only textarea").waitUntilExists(function() {
