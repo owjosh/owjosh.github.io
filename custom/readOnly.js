eed1f35377b097c9bebfdb5933b9f0ab7640d5c8
Joshua Wray <joshua@getopenwater.com>
2020-09-09 20:05:46 -0500

    no message

--- custom/readOnly.js
+++ custom/readOnly.js
@@ -1,10 +1,9 @@
-// jshint esversion: 6
 $('head').append($('<' + 'link rel="stylesheet" type="text/css" />').attr('href', 'https://owjosh.github.io/custom/read-only.css'));
 
 $(function() {
-    $.getScript("https://openwater-themes.s3.amazonaws.com/js/waitForElement.js", function(data, textStatus, jqxhr) {
+    $.getScript("https://openwater-themes.s3.amazonaws.com/js/waitForElement.js", function() {
         $('.read-only select').waitUntilExists(function() {
-            $('.read-only select').attr("disabled", true);
+            $('.read-only select').attr = ("disabled", true);
             this.tabIndex = -1;
         })
         $(".read-only input[type='text'], .read-only textarea").waitUntilExists(function() {
