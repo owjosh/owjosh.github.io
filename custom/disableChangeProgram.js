9b43394daaa8ddd54358ae08afa1e5c78e4bc0e6
Joshua Wray <joshua@getopenwater.com>
2020-09-20 21:01:29 -0500

    no message

--- custom/disableChangeProgram.js
+++ custom/disableChangeProgram.js
@@ -1,15 +1,15 @@
-$(".changeSolicitation").parents("li").hide();
-$(".currentSolicitation").hide();
-$.get('/a/organizations/main/solicitations/selectcurrentsolicitation', function(data) {
-    $(data).find("script:contains('currentSolicitationId')").each(function() {
-        var checkers = $(this).text().slice(127, 129);
-        if (checkers != '""') {
-            Backbone.post('/a/organizations/main/solicitations/selectcurrentsolicitation', {
-                    solicitationId: ''
-                }, { dontShowSuccessAlert: !0 }, )
-                .done(function() {
-                    window.location.assign(window.location.href)
-                })
-        }
-    })
-});
+// $(".changeSolicitation").parents("li").hide();
+// $(".currentSolicitation").hide();
+// $.get('/a/organizations/main/solicitations/selectcurrentsolicitation', function(data) {
+//     $(data).find("script:contains('currentSolicitationId')").each(function() {
+//         var checkers = $(this).text().slice(127, 129);
+//         if (checkers != '""') {
+//             Backbone.post('/a/organizations/main/solicitations/selectcurrentsolicitation', {
+//                     solicitationId: ''
+//                 }, { dontShowSuccessAlert: !0 }, )
+//                 .done(function() {
+//                     window.location.assign(window.location.href)
+//                 })
+//         }
+//     })
+// });
--- custom/disableChangeProgramV2.js
+++ custom/disableChangeProgramV2.js
@@ -1,22 +1,22 @@
-(function() {
-    'use strict';
-    var URL = '/a/organizations/main/solicitations/selectcurrentsolicitation';
-    $('#warp').addClass('change-solicitation-fix');
-    $(".changeSolicitation").parents("li").hide();
-    $(".currentSolicitation").hide();
-    $.get(URL, function(data) {
-        $(data).find("script:contains('currentSolicitationId')").each(function() {
-            var REGEX = /currentSolicitationId: "(\d+)"/;
-            if (REGEX.test(this.innerText) && !!this.innerText.match(REGEX)[1]) {
-                Backbone.post(URL, {
-                        solicitationId: ''
-                    }, {
-                        dontShowSuccessAlert: !0
-                    })
-                    .done(function() {
-                        window.location.assign(window.location.href)
-                    });
-            }
-        });
-    });
-})();
+// (function() {
+//     'use strict';
+//     var URL = '/a/organizations/main/solicitations/selectcurrentsolicitation';
+//     $('#warp').addClass('change-solicitation-fix');
+//     $(".changeSolicitation").parents("li").hide();
+//     $(".currentSolicitation").hide();
+//     $.get(URL, function(data) {
+//         $(data).find("script:contains('currentSolicitationId')").each(function() {
+//             var REGEX = /currentSolicitationId: "(\d+)"/;
+//             if (REGEX.test(this.innerText) && !!this.innerText.match(REGEX)[1]) {
+//                 Backbone.post(URL, {
+//                         solicitationId: ''
+//                     }, {
+//                         dontShowSuccessAlert: !0
+//                     })
+//                     .done(function() {
+//                         window.location.assign(window.location.href)
+//                     });
+//             }
+//         });
+//     });
+// })();
